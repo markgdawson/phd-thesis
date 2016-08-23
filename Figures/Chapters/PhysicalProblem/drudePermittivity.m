ca21e19c3e1ab927a306b0b03d7afd313c1d0e70
Mark Dawson <markgdawson@gmail.com>
2016-08-23 15:47:21 +0100

    Ruben suggested changes Chapter 2

--- Figures/Chapters/PhysicalProblem/drudePermittivity.m
+++ Figures/Chapters/PhysicalProblem/drudePermittivity.m
@@ -14,25 +14,25 @@ set(gca,'fontsize',txtSize)
 plot(omega./omega_p, re./epsilon_inf,'k','LineWidth',1.5)
 set(gca,'XTick',[1]);
 set(gca,'YTick',[1]);
-set(gca,'XTickLabel','$\omega_{p}$');
-set(gca,'YTickLabel','$\varepsilon_{\infty}$');
+set(gca,'XTickLabel','1');
+set(gca,'YTickLabel','1');
 grid on
 % set(gca,'LineWidth', 1);
 % set(gca,'GridLineStyle', '-');
-ylabel('$Re\{ \varepsilon_r \}$')
-xlabel('$\omega$')
+ylabel('$\textrm{Im}\{ \varepsilon_r \} / \varepsilon_{\infty}$')
+xlabel('$\omega / \omega_{p}$')
 xlim([0,xmax])
 ylim([0,1.1])
 latexExportFigure('file','drudePermittivityReal');
 figure;
 set(gca,'fontsize',txtSize)
 plot(omega./omega_p, imag./epsilon_inf,'k','LineWidth',1.5)
-ylabel('$Im\{ \varepsilon_r \}$')
-xlabel('$\omega$')
+ylabel('$\textrm{Re}\{ \varepsilon_r \} / \varepsilon_{\infty}$')
+xlabel('$\omega / \omega_{p}$')
 grid on
-set(gca,'XTick',[1]);
-set(gca,'YTick',[1]);
-set(gca,'XTickLabel','$\omega_{p}$');
-set(gca,'YTickLabel','$\varepsilon_{\infty}$');
+set(gca,'XTick',1);
+set(gca,'YTick',1);
+set(gca,'XTickLabel','1');
+set(gca,'YTickLabel','1');
 xlim([0,xmax])
 latexExportFigure('file','drudePermittivityImag');
